479b8d4fda4f586d5f935e9c1fb7d2be0ae4c47b
tomdozz <tom_1995@msn.com>
2018-05-21 14:38:08 +0200

    added and removed some testt in unittest.php

---
+++ tests/UtilsTest.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+require dirname(__DIR__) . '/src/Utils.php';
+
+final class UtilsTest extends TestCase
+{
+    public function testGetNameWithValidValue()
+    {
+        $utils = new Utils();
+        $this->assertEquals("Axel", $utils->get_name(1981, 6, 16));
+        //$this->assertEquals("Axel", $utils->get_insult(tom));
+    }
+}
